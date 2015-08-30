6f6765ec9848bdfc74541233c39152674e1e515a
TBVanderley4545 <tbvanderley@gmail.com>
2015-08-30 09:55:02 -0400

    Finished Intent
    This update contains the finished creation of the intent through page
    93.

--- app/src/main/java/com/tylervanderley/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/tylervanderley/messenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
@@ -15,7 +16,17 @@ public class CreateMessageActivity extends Activity {
 
     // Call onSendMessage() when the button is clicked
     public void onSendMessage(View view) {
+        // When we click the button, the following happens. 1) The activity looks in the layout to
+        // find the EditText with an id of message. 2) The text object is taken from the message
+        // view and converted to a string in a String object called messageText. 3) Create a new
+        // intent called intent that links to the ReceiveMessageActivity. 4) use the intent.putExtra()
+        // method to add the messageText to the intent in an object called "message". 5) Start the
+        // next activity by passing in the previously created intent.
+
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/tylervanderley/messenger/ReceiveMessageActivity.java
+++ app/src/main/java/com/tylervanderley/messenger/ReceiveMessageActivity.java
@@ -1,37 +1,22 @@
 package com.tylervanderley.messenger;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.TextView;
 
-public class ReceiveMessageActivity extends AppCompatActivity {
+public class ReceiveMessageActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.message);
+        messageView.setText(messageText);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_receive_message, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
